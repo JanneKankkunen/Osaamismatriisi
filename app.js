569d282e2faa97a8fede3c3cc39207764a0c5c66
Turuntom <tomi.turunen@edu.savonia.fi>
2019-12-01 06:43:33 +0200

    Lisätty mm. rajapintaan lisää hakuja tietokantaan ja visuaalisuutta osaamismatriisiin.

--- app.js
+++ app.js
@@ -35,16 +35,19 @@ var kayttajanLinja = "XlinjaX";
 var kayttajanKompetenssit = [];
 var kayttajanKurssitJaArvosanat = [];
 
-var kompetenssiObj = {
-    nimi: "kompetenssi",
-    suoritukset: [
-        {
-            kurssi: "kurssi1",
-            arvosana: "5" 
-        }
-    ]
-};
+//kompetenssi objektiin pitäis saada:
+//  Kompetenssin sisältämien kurssien lukumäärä
+//  Käyttäjän yhteisarvosana kursseista, joita on suorittanut kompetenssin sisältä
+//  Kompetenssi nimi
+// 
 
+var kompetenssiObj1 = {}
+var kompetenssiObj2 = {}
+var kompetenssiObj3 = {}
+var kompetenssiObj4 = {}
+var kompetenssiObj5 = {}
+
+var kompetenssiObjt = [kompetenssiObj1,kompetenssiObj2,kompetenssiObj3, kompetenssiObj4, kompetenssiObj5]
 
 
 app.post('/auth', (request, response) => {
@@ -65,7 +68,7 @@ app.post('/auth', (request, response) => {
         'AND kayttajaTunnus="'+username+'" AND salasana= "'+password+'"'
 
 
-        const kysely = ' SELECT * FROM kayttaja WHERE kayttajaTunnus="'+username+'" AND salasana= "'+password+'"'
+        
         
 		connection.query(megaKysely, (error, results) => {
             if (results.length > 0) {
@@ -83,30 +86,77 @@ app.post('/auth', (request, response) => {
                     if(kayttajanKompetenssit.indexOf(element.Kompetenssi) === -1){
                         kayttajanKompetenssit.push(element.Kompetenssi)
                     }
-                    console.log(element.Kompetenssi)
                 });
+                
+                kompetenssiObj1.nimi = kayttajanKompetenssit[0]
+                kompetenssiObj2.nimi = kayttajanKompetenssit[1]
+                kompetenssiObj3.nimi = kayttajanKompetenssit[2]
+                kompetenssiObj4.nimi = kayttajanKompetenssit[3]
+                kompetenssiObj5.nimi = kayttajanKompetenssit[4]
                 // kayttajanTiedotArrayssa = JSON.stringify(results.array.forEach(element => {
                 //     console.log(element)
                 // }));
+                
+                kompetenssiObjt.forEach(kompetenssi => {
+                    kompetenssi.kayttajanArvosanatYhteensa = 0
+                    results.forEach(element => {
+                        if(kompetenssi.nimi === element.Kompetenssi){
+                            console.log(element.arvosana)
+                            kompetenssi.kayttajanArvosanatYhteensa += element.arvosana
+                        }
+                    })
+                })
+
 
 				response.redirect('/home');
                 
             } else {
 				response.send('Incorrect Username and/or Password!');
             }
-		});
+        });
+        
+        const kysely = ' SELECT kompetenssiNimi, kompetenssinkurssit.kompetenssiID, COUNT(kurssiID) kurssienLkm FROM kompetenssinkurssit '+
+        'INNER JOIN kompetenssi ON kompetenssi.kompetenssiID = kompetenssinKurssit.kompetenssiID GROUP BY kompetenssiID '
+
+        connection.query(kysely,(error,results) => {
+            if (results.length > 0) {
+				request.session.loggedin = true;
+                request.session.username = username;
+
+                results.forEach(element => {
+                    kompetenssiObjt.forEach(kompetenssiObj => {
+                        if(kompetenssiObj.nimi === element.kompetenssiNimi){
+                            kompetenssiObj.maxKurssit = element.kurssienLkm
+                        }
+                    })
+                })
+
+                kompetenssiObjt.forEach(kompe => {
+                    kompe.maxArvosanat = kompe.maxKurssit * 5
+                })
+
+                kompetenssiObjt.forEach(kompe => {
+                    kompe.kompetenssiProsentti = kompe.kayttajanArvosanatYhteensa / kompe.maxArvosanat * 100
+                })
+
+            }
+        })
+
+
 	} else {
 		response.send('Please enter Username and Password!');
 		response.end();
 	}
 });
 
+
+
+
 app.get('/home',(request,response) => {
 
     if(request.session.loggedin){
         //response.send('Welcome back, '+ request.session.username + '!')
         //response.sendFile(__dirname+"/index.html")
-        var objString = JSON.stringify(kompetenssiObj)
 
         response.render('index', {
             kayttaja: username,
@@ -114,7 +164,16 @@ app.get('/home',(request,response) => {
             kayttajanNimi: kayttajanNimi,
             kayttajanLinja: kayttajanLinja,
             kayttajanKompetenssit: kayttajanKompetenssit,
-            kompetenssiObj: objString
+            kompetenssiObj1: kompetenssiObj1,
+            kompetenssiObj2: kompetenssiObj2,
+            kompetenssiObj3: kompetenssiObj3,
+            kompetenssiObj4: kompetenssiObj4,
+            kompetenssiObj5: kompetenssiObj5
+            , helpers: {
+                json: function(context){
+                    return JSON.stringify(context)
+                }
+            }
         })
     }else{
         response.send('Please login to view this page!')
